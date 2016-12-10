c913780951edcc64c89a70174a39cd0d3bce4404
Yash <yashpatel256@gmail.com>
2016-12-10 17:06:49 -0500

    Creation of angular module

--- public/app.js
+++ public/app.js
@@ -1,3 +1,17 @@
 /**
  * Created by Yash on 12/10/2016.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('InstaApp', ['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/gallery', {
+                    templateUrl: 'views/view.gallery.html',
+                    controller: 'GalleryCtrl'
+                })
+                .otherwise({redirectTo: '/gallery'});
+        }]);
+})();
