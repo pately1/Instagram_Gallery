7f1153d44c8bcf2c6e7684e20e9c96102abb0073
Yash <yashpatel256@gmail.com>
2016-12-10 17:08:11 -0500

    Creeation of gallery controller

---
+++ public/controllers/gallery.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Yash on 12/10/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('InstaApp')
+        .controller('GalleryCtrl', ['$scope', function ($scope) {
+
+        }]);
+
+})();
