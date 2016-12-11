17c56e6d0c062ccb4b4060e130343f612b306263
Yash <yashpatel256@gmail.com>
2016-12-11 17:13:43 -0500

    extracting just images from the entire array. angular-lightbox functionality to show the clicked images in a moda view

--- public/controllers/gallery.controller.js
+++ public/controllers/gallery.controller.js
@@ -6,8 +6,22 @@
 
     angular
         .module('InstaApp')
-        .controller('GalleryCtrl', ['$scope', function ($scope) {
-
+        .controller('GalleryCtrl', ['$scope','$http','Lightbox', function ($scope, $http, Lightbox) {
+            $scope.images = [];
+            var imgArray = [];
+            console.log("In Controller change");
+            $http.jsonp('https://api.instagram.com/v1/users/self/media/recent/?access_token=YOUR_ACCESS_TOKEN')
+                .then(function (response) {
+                    $scope.images = response.data.data;
+                    angular.forEach($scope.images, function (val) {
+                       imgArray.push(val.images.standard_resolution);
+                    });
+                    $scope.openLightboxModal = function (index) {
+                        Lightbox.openModal(imgArray, index);
+                    }
+                },(function (error) {
+                    console.log(error);
+                }));
         }]);
 
 })();
